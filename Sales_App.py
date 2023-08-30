e03057b663dd0246674b93af37404a18d5535223
Arawindha Reddy <38184176+Aravind367@users.noreply.github.com>
2023-08-30 17:18:23 +0530

    Update Sales_App.py

--- Sales_App.py
+++ Sales_App.py
@@ -18,7 +18,7 @@ if fil is not None:
     st.write(filename)
     df = pd.read_csv(filename, encoding="ISO-8859-1")
 else:
-    os.chdir(r"C:\Users\ARAVIND\Downloads\MC_Streamlit")
+    #os.chdir(r"C:\Users\ARAVIND\Downloads\MC_Streamlit")
     df = pd.read_csv("Superstore.csv", encoding="ISO-8859-1")
 
 col1, col2 = st.columns((2))
